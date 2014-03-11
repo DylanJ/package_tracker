2cb7f99565d2153b0256ecf16d8198a204317d90
Dylan Johnston <qdylanj@gmail.com>
2014-03-10 17:14:32 -0700

    Insert modal dialog when tracker_button is called

--- app/helpers/package_tracker/tracker_helper.rb
+++ app/helpers/package_tracker/tracker_helper.rb
@@ -1,9 +1,8 @@
 module PackageTracker
   module TrackerHelper
     def tracker_button(id="123")
-      content_tag(:div, class: 'foo') do
-        "hello world"
-      end
+      modal_id = "modal#{Object.new.hash}"
+      render('package_tracker/tracking/modal', modal_id: modal_id)
     end
   end
 end
