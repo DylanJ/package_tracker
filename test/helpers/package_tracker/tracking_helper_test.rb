bbf212f0be4989aeff92e8b921093a88b9c2481a
Dylan Johnston <qdylanj@gmail.com>
2014-03-12 03:17:39 -0700

    Remove test dir

--- test/helpers/package_tracker/tracking_helper_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-module PackageTracker
-  class TrackingHelperTest < ActionView::TestCase
-  end
-end
--- test/package_tracker_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PackageTrackerTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, PackageTracker
-  end
-end
