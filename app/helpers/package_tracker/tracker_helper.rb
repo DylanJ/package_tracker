2e4a3ea20ef5f90c11a14d7c8af4092facea40c7
Dylan Johnston <qdylanj@gmail.com>
2014-03-13 22:57:38 -0700

    clean up helper

--- app/helpers/package_tracker/tracker_helper.rb
+++ app/helpers/package_tracker/tracker_helper.rb
@@ -1,8 +1,11 @@
 module PackageTracker
   module TrackerHelper
-    def tracker_button id
-      modal_id = "modal#{Object.new.hash}"
+    def tracker_button id=nil
       render('package_tracker/tracking/modal', modal_id: modal_id, id: id)
+    end
+
+    def modal_id
+      "modal#{Object.new.hash}"
     end
   end
 end
--- spec/helpers/package_tracker_helper_spec.rb
+++ spec/helpers/package_tracker_helper_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe PackageTracker::TrackerHelper do
   describe 'tracker_button' do
-    it "should render modal partial" do
+    before(:each) do
       helper.stub(:modal_id).and_return(1)
-      helper.should_receive(:render).with('package_tracker/tracking/modal', modal_id: 1)
-      helper.tracker_button()
+      helper.should_receive(:render).with('package_tracker/tracking/modal', modal_id: 1, id: id)
+    end
+    context "with an argument" do
+      let(:id) { '123' }
+      it "should render modal partial" do
+        helper.tracker_button( id )
+      end
+    end
+
+    context "without an argument" do
+      let(:id) { nil }
+      it "should render modal partial" do
+        helper.tracker_button()
+      end
     end
   end
 end
