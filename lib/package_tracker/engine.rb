79cadd3b772609406fd52c94e1c315785f61d925
Dylan Johnston <qdylanj@gmail.com>
2014-03-15 03:00:31 -0700

    configs aren't used any more

--- lib/package_tracker/engine.rb
+++ lib/package_tracker/engine.rb
@@ -1,12 +1,6 @@
-require 'package_tracker/config'
-
 module PackageTracker
   class Engine < ::Rails::Engine
     isolate_namespace PackageTracker
-
-    initializer "package_tracker.load_config" do
-      PackageTracker::Config.load_config
-    end
 
     initializer "package_tracker.load_helpers" do
       ActiveSupport.on_load :action_controller do
