03ded46e757c1f9ab1658a4112f14cbb38052502
Dylan Johnston <qdylanj@gmail.com>
2014-03-15 03:32:31 -0700

    clean up dhl carrier a bit

--- lib/package_tracker/dhl.rb
+++ lib/package_tracker/dhl.rb
@@ -5,10 +5,11 @@ module PackageTracker
       StatusRow = 2         # third row in tbody
       StatusTextCell = 2    # third cell in <td>
       StatusTextElement = 2 # "text<br>status"
+      Headers = [:date, :location, :status]
 
       def self.lookup id
-
-        url = "http://nolp.dhl.de/nextt-online-public/set_identcodes.do?lang=#{I18n.locale.to_s}&idc=#{id}&extendedSearch=true"
+        lang = I18n.locale.to_s
+        url = "http://nolp.dhl.de/nextt-online-public/set_identcodes.do?lang=#{lang}&idc=#{id}&extendedSearch=true"
         doc = Nokogiri::HTML(open(url))
 
         dhl_table = doc.css('table')
@@ -17,16 +18,14 @@ module PackageTracker
           return { response: 'failure', message: I18n.t('package_tracker.no_id') }
         end
 
-        status = clean(dhl_table.css('tbody tr')[StatusRow].css('td')[StatusTextCell].children[StatusTextElement].content)
+        raw_status = dhl_table.css('tbody tr')[StatusRow].css('td')[StatusTextCell].children[StatusTextElement].content
+        status = clean(raw_status)
 
         updates_table = doc.css('table table')
-
-        headers = [:date, :location, :status]
-
         updates = updates_table.css('tbody tr').map do |row|
           result = {}
           cols = row.css('td').map { |cell| clean(cell.content) }
-          headers.each_with_index { |h, i| result[h] = cols[i] }
+          Headers.each_with_index { |h, i| result[h] = cols[i] }
 
           result
         end
@@ -39,10 +38,10 @@ module PackageTracker
         }
       end
 
+      # strip tabs and newlines
       def self.clean(text)
         text.gsub!(/\t|\n?/, '')
       end
-
     end
   end
 end
